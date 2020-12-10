a30543eae41905e4d69a28f4dc0066c4f1c3d476
“xuyuzhiyue <daijinbo@thing-data.com>
2020-12-11 00:54:15 +0800

    提交手机相机类型的后台数据接口

--- goodsApi/public/js/allgoods.js
+++ goodsApi/public/js/allgoods.js
@@ -37,6 +37,13 @@ let OverClothes
 // 苏宁房产
 let suifan
 
+// 手机照片
+let iphonMajorSuit
+let iphonOpportunity
+let iphonParts
+let iphonWebServer
+let iphonPhotography
+let iphonCameraAcc
 
 function allMysqlData(res) {
 
@@ -193,11 +200,11 @@ function allMysqlData(res) {
         if (err) throw res.send({ err_code: 1, message: '数据不存在' });
         OverNutrition = {
             "cat_id": 112,
-			"cat_name": "营养保健",
-			"cat_pid": 71,
-			"cat_level": 1,
-			"cat_deleted": false,
-			"cat_icon": "/full/none.jpg",
+            "cat_name": "营养保健",
+            "cat_pid": 71,
+            "cat_level": 1,
+            "cat_deleted": false,
+            "cat_icon": "/full/none.jpg",
             "children": result
         }
     })
@@ -208,12 +215,12 @@ function allMysqlData(res) {
     connection.query(sqlFoots, (err, result) => {
         if (err) throw res.send({ err_code: 1, message: '数据不存在' });
         OverFoots = {
-			"cat_id": 119,
-			"cat_name": "食品",
-			"cat_pid": 71,
-			"cat_level": 1,
-			"cat_deleted": false,
-			"cat_icon": "/full/none.jpg",
+            "cat_id": 119,
+            "cat_name": "食品",
+            "cat_pid": 71,
+            "cat_level": 1,
+            "cat_deleted": false,
+            "cat_icon": "/full/none.jpg",
             "children": result
         }
     })
@@ -223,12 +230,12 @@ function allMysqlData(res) {
     connection.query(sqlDig, (err, result) => {
         if (err) throw res.send({ err_code: 1, message: '数据不存在' });
         OverDigital = {
-			"cat_id": 134,
-			"cat_name": "数码家电",
-			"cat_pid": 71,
-			"cat_level": 1,
-			"cat_deleted": false,
-			"cat_icon": "/full/none.jpg",
+            "cat_id": 134,
+            "cat_name": "数码家电",
+            "cat_pid": 71,
+            "cat_level": 1,
+            "cat_deleted": false,
+            "cat_icon": "/full/none.jpg",
             "children": result
         }
     })
@@ -239,12 +246,12 @@ function allMysqlData(res) {
     connection.query(sqlCloth, (err, result) => {
         if (err) throw res.send({ err_code: 1, message: '数据不存在' });
         OverClothes = {
-			"cat_id": 155,
-			"cat_name": "服饰箱包",
-			"cat_pid": 71,
-			"cat_level": 1,
-			"cat_deleted": false,
-			"cat_icon": "/full/none.jpg",
+            "cat_id": 155,
+            "cat_name": "服饰箱包",
+            "cat_pid": 71,
+            "cat_level": 1,
+            "cat_deleted": false,
+            "cat_icon": "/full/none.jpg",
             "children": result
         }
     })
@@ -256,14 +263,110 @@ function allMysqlData(res) {
         if (err) throw res.send({ err_code: 1, message: '数据不存在' });
         suifan = {
             "cat_id": 171,
-			"cat_name": "苏宁房产",
-			"cat_pid": 170,
-			"cat_level": 1,
-			"cat_deleted": false,
-			"cat_icon": "/full/none.jpg",
+            "cat_name": "苏宁房产",
+            "cat_pid": 170,
+            "cat_level": 1,
+            "cat_deleted": false,
+            "cat_icon": "/full/none.jpg",
             "children": result
         }
     })
+
+
+    // 手机照片
+    // 获取大牌推荐数据
+    const sqlMajor = 'select * from allgoods where isdel = 0 and cat_type = "大牌推荐"'
+    connection.query(sqlMajor, (err, result) => {
+        if (err) throw res.send({ err_code: 1, message: '数据不存在' });
+        iphonMajorSuit = {
+            "cat_id": 184,
+            "cat_name": "大牌推荐",
+            "cat_pid": 172,
+            "cat_level": 1,
+            "cat_deleted": false,
+            "cat_icon": "/full/none.jpg",
+            "children": result
+        }
+    })
+
+    // 获取搞机达人数据
+    const sqlOpporr = 'select * from allgoods where isdel = 0 and cat_type = "搞机达人"'
+    connection.query(sqlOpporr, (err, result) => {
+        if (err) throw res.send({ err_code: 1, message: '数据不存在' });
+        iphonOpportunity = {
+            "cat_id": 184,
+            "cat_name": "大牌推荐",
+            "cat_pid": 172,
+            "cat_level": 1,
+            "cat_deleted": false,
+            "cat_icon": "/full/none.jpg",
+            "children": result
+        }
+    })
+
+    // 获取手机配件数据
+    const sqlParts = 'select * from allgoods where isdel = 0 and cat_type = "手机配件"'
+    connection.query(sqlParts, (err, result) => {
+        if (err) throw res.send({ err_code: 1, message: '数据不存在' });
+        iphonParts = {
+            "cat_id": 199,
+            "cat_name": "手机配件",
+            "cat_pid": 172,
+            "cat_level": 1,
+            "cat_deleted": false,
+            "cat_icon": "/full/none.jpg",
+            "children": result
+        }
+    })
+
+
+    // 获取手机服务数据
+    const sqlServer = 'select * from allgoods where isdel = 0 and cat_type = "手机服务"'
+    connection.query(sqlServer, (err, result) => {
+        if (err) throw res.send({ err_code: 1, message: '数据不存在' });
+        iphonWebServer = {
+            "cat_id": 212,
+            "cat_name": "手机服务",
+            "cat_pid": 172,
+            "cat_level": 1,
+            "cat_deleted": false,
+            "cat_icon": "/full/none.jpg",
+            "children": result
+        }
+    })
+
+
+        // 获取摄影摄像数据
+        const sqlPhotography = 'select * from allgoods where isdel = 0 and cat_type = "摄影摄像"'
+        connection.query(sqlPhotography, (err, result) => {
+            if (err) throw res.send({ err_code: 1, message: '数据不存在' });
+            iphonPhotography = {
+                "cat_id": 228,
+                "cat_name": "摄影摄像",
+                "cat_pid": 172,
+                "cat_level": 1,
+                "cat_deleted": false,
+                "cat_icon": "/full/none.jpg",
+                "children": result
+            }
+        })
+
+                // 获取相机配件数据
+                const sqlCameraAcc = 'select * from allgoods where isdel = 0 and cat_type = "相机配件"'
+                connection.query(sqlCameraAcc, (err, result) => {
+                    if (err) throw res.send({ err_code: 1, message: '数据不存在' });
+                    iphonCameraAcc = {
+                        "cat_id": 245,
+                        "cat_name": "相机配件",
+                        "cat_pid": 172,
+                        "cat_level": 1,
+                        "cat_deleted": false,
+                        "cat_icon": "/full/none.jpg",
+                        "children": result
+                    }
+                })
+
+
 
     //   总的拼接
     allData = {
@@ -291,14 +394,22 @@ function allMysqlData(res) {
             "cat_deleted": false,
             "cat_icon": "/full/none.jpg",
             "children": [OverInter, OverMotherInfant, OverMakeup, OverPersonalCare, OverNutrition, OverFoots, OverDigital, OverClothes]
-        },{
+        }, {
             "cat_id": 170,
             "cat_name": "苏宁房产",
             "cat_pid": 0,
             "cat_level": 0,
             "cat_deleted": false,
             "cat_icon": "/full/none.jpg",
-            "children":[suifan]
+            "children": [suifan]
+        }, {
+            "cat_id": 172,
+            "cat_name": "手机相机",
+            "cat_pid": 0,
+            "cat_level": 0,
+            "cat_deleted": false,
+            "cat_icon": "/full/none.jpg",
+            "children": [iphonMajorSuit, iphonOpportunity, iphonParts, iphonWebServer,iphonPhotography,iphonCameraAcc]
         }]
     }
     res.send(allData)
