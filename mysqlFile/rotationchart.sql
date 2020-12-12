7f4456d6e85f2d6eebf08eea9eb75981ff71ac1a
“xuyuzhiyue <daijinbo@thing-data.com>
2020-12-12 16:54:15 +0800

    数据库导出的sql格式文件

---
+++ mysqlFile/rotationchart.sql
@@ -0,0 +1,3 @@
+INSERT INTO `rotationchart` VALUES (38, 'http://127.0.0.1:8800/rotat/banner3', 'navigate', '/pages/goods_detail/main?goods_id=38', 0);
+INSERT INTO `rotationchart` VALUES (129, 'http://127.0.0.1:8800/rotat/banner2', 'navigate', '/pages/goods_detail/main?goods_id=129', 0);
+INSERT INTO `rotationchart` VALUES (395, 'http://127.0.0.1:8800/rotat/banner1', 'navigate', '/pages/goods_detail/main?goods_id=395', 0);
---
+++ mysqlFile/user.sql
@@ -0,0 +1,7 @@
+INSERT INTO `user` VALUES (1, 'zhansan', 18, 0, '2020-10-10');
+INSERT INTO `user` VALUES (2, 'lisi', 20, 1, '2020-12-9');
+INSERT INTO `user` VALUES (3, '王五11', 888, 0, '2020-12-09 18:25:11');
+INSERT INTO `user` VALUES (4, '77', 33, 1, '2020-12-09 18:52:52');
+INSERT INTO `user` VALUES (7, '老刘', 10, 0, '2020-12-09 20:49:47');
+INSERT INTO `user` VALUES (8, 'admin', 18, 0, '2020-12-09 22:03:15');
+INSERT INTO `user` VALUES (9, 'DW', 2020, 0, '2020-12-10 15:44:02');
