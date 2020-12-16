e7aee21f3be098dcfa047afc07cb61a8efc13df7
“xuyuzhiyue <daijinbo@thing-data.com>
2020-12-16 17:38:55 +0800

    提交意见反馈代码

--- AppletsProject/pages/feedback/index.js
+++ AppletsProject/pages/feedback/index.js
@@ -5,62 +5,123 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    tabs:[
+      {
+        id:0,
+        value:"商品问题意见",
+        isActive:true
+      },
+      {
+        id:1,
+        value:"其他投诉",
+        isActive:false
+      }
+    ],
+    // 被选中的图片路径数组
+    chooseImage:[],
+    // 文本字符串
+    textVal:''
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  // 外网图片路径数组
+  UpLoadImge:[],
+ //标题的点击事件 从子组件传递过来
+  handleTabsItemChange(e){
+    // 获取被点击的标题索引  
+    const index  = e.detail
+  // // 修改源数组  
+   let {tabs} = this.data
+   tabs.forEach((v,i)=>i===index?v.isActive=true:v.isActive=false)
+   this.setData({
+     tabs
+   })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 获取图片
+  handleChooseImg(){
+    wx.chooseImage({
+      count:9,
+      sizeType:['original','compressed'],
+      sourceType:['album','camera'],
+      success:result=>{
+        this.setData({
+          // 把图片数组进行拼接
+          chooseImage:[...this.data.chooseImage,...result.tempFilePaths]
+        })
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  // 点击自定义组件删除
+  handleRemoveImg(e){
+    const {index} = e.currentTarget.dataset;
+    let {chooseImage} = this.data
+    chooseImage.splice(index,1)
+    this.setData({
+      chooseImage
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  // 文本域
+  handleTextInput(e){
+    this.setData({
+      textVal:e.detail.value
+    })
   },
+  // 提交按钮点击事件
+  handleFormSubmit(){
+    // 获取文本域内容
+    const {textVal,chooseImage} = this.data
+    // 合法性校验
+    if(!textVal.trim()){
+      // 不合法
+      wx.showToast({
+        title: '输入不合法',
+        icon:'none',
+        mask:true
+      })
+      return;
+    }
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+    // 显示正在等待图片
+    wx.showLoading({
+      title: '正在上传',
+      mask:true
+    })
+    // 需要把图片上传到专门的图片服务器
+    if(chooseImage.length != 0 ){
+      chooseImage.forEach((v,i) => {
+      wx.uploadFile({
+          filePath: v,
+          name: 'file',
+          url: 'https://images.ac.cn/home/Index/UploadAction/',
+          formData:{},
+          success:result=>{
+            // console.log(result);
+            // let url = Json.parse(result.data)
+            // this.UpLoadImge.push(url)
 
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+            //所有的图片都上传完毕了才触发
+            if(i===chooseImage.length-1){
+              wx.hideLoading()
+              // 提交到后台中
+              wx.showToast({
+                title: '已经提交成功',
+              })
+              // console.log('');
+              // 重置页面
+              this.setData({
+                textVal:'',
+                chooseImage:[]
+              })
+              // 返回上一个页面
+              wx.navigateBack({
+                delta:1
+              })
+            }
+          }
+      })
+      })
+    }else{
+      wx.showToast({
+        title: '已经提交成功',
+      })
+    }
   }
 })
